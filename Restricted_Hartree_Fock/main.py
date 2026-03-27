06a8552026a9fabc5c93b040068abddebeba2a21
Nigiha <h_kohakusan589@icloud.com>
2026-03-27 17:08:04 +0900

    step2まで

--- Restricted_Hartree_Fock/main.py
+++ Restricted_Hartree_Fock/main.py
@@ -1,7 +1,12 @@
+import numpy as np
+from scipy.integrate import quad
+
 #==========step1:対象分子の設定==========
 
-target_molecule="HeH"
+
+target_molecule_file="HeH.xyz"
 base_function_file="STO-3G.json"
+
 
 # 元素記号から原子番号への変換辞書リスト
 symbol_to_Z = {
@@ -18,9 +23,43 @@ symbol_to_Z = {
 }
 
 
+
 #核電荷{Z}と核座標{R=(x,y,z)}の受け取り
-from molecule import molecule_structure
-molecule=molecule_structure[target_molecule]
+
+angstrom_to_bohr=1.8897259886
+
+
+def load_xyz(xyz_file, to_bohr=True):
+    molecule_data=[]
+
+    with open(xyz_file, "r", encoding="utf-8") as f:
+        lines=f.readlines()
+    
+    num_atoms=int(lines[0].strip())
+
+    for line in lines[2:]:
+        if not line.strip():
+            continue
+
+        parts=line.split()
+        symbol=parts[0]
+        x, y, z=float(parts[1]), float(parts[2]), float(parts[3])
+
+        if to_bohr:
+            x*=angstrom_to_bohr
+            y*=angstrom_to_bohr
+            z*=angstrom_to_bohr
+
+        molecule_data.append({
+            "symbol": symbol,
+            "coord":np.array([x, y, z])
+        })
+
+    return molecule_data, num_atoms
+    
+molecule, N=load_xyz(target_molecule_file, to_bohr=True)
+
+
 
 #基底関数の受け取り(STO-3G.jsonからGTOの指数と係数のデータを抜粋)
 import json
@@ -48,6 +87,7 @@ def load_atom_basis(json_file, atom):
     return extracted_shells
 
 
+
 #分子全体の基底関数リストを作る
 def build_molecule_basis(molecule, json_file):
     molecule_basis=[]
@@ -70,82 +110,86 @@ def build_molecule_basis(molecule, json_file):
     
     return molecule_basis
 
-basis=build_molecule_basis(molecule, "STO-3G.json")
-print(basis)
+molecule_basis=build_molecule_basis(molecule, base_function_file)
+# print(molecule_basis)
 
 
 
+#核間反発エネルギーV_nucの計算
+# def V_nuc_matrix(molecule):
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import numpy as np
-from scipy.integrate import quad
-
-# #核間反発エネルギーV_nucの計算
-# def V_nuc(N, mol_str):
-#     Z, x, y, z=mol_str[0,:], mol_str[1,:], mol_str[2,:], mol_str[3,:]
 #     V=np.zeros((N, N))
+    
 #     for i in range(N):
 #         for j in range(i+1, N):
-#             r_x=x[i]-x[j]
-#             r_y=y[i]-y[j]
-#             r_z=z[i]-z[j]
-#             r=np.sqrt(r_x**2+r_y**2+r_z**2)
-#             if r==0:
-#                 print("エラー：")
-#                 exit()
-#             V[i][j]=Z[i]*Z[j]/r
-#             V[i][j]=Z[i]*Z[j]/r
+#             r_i, r_j=np.array(molecule[i]["coord"]), np.array(molecule[j]["coord"])
+#             Z_i, Z_j=symbol_to_Z[molecule[i]["symbol"]], symbol_to_Z[molecule[j]["symbol"]]
+#             V[i][j]=Z_i*Z_j/(np.linalg.norm(r_i-r_j))
+#             V[j][i]=Z_i*Z_j/(np.linalg.norm(r_i-r_j))
+    
 #     return V
 
-# #==========step2:正準直交化の実行==========
-# # 重なり行列Sの計算
+def V_nuc_schalar(molecule):
 
-# #Sの対角化
-
-# #変換行列Xの計算(正準直交化)                 
-
-# #==========step3:コアハミルトニアン行列の計算==========
-
-# #運動エネルギー項Tの計算
-# def T(N, a_i):
-#     def GTO(a):
-#         return lambda r:(2*a/np.pi)**(3/4)*np.exp(-a*r**2)
-
-#     def laplace_GTO(a):
-#         return lambda r:(2*a/np.pi)**(3/4) * 2*a(2*a*r**2-3)*np.exp(-a*r**2)
-
-#     J=lambda r:4*np.pi*r**2
+    V=0
     
-#     T=np.zeros((N, N))
-#     for i in range(N):
-#         for j in range(N):
-#             T[i][j]=quad(GTO(a_i[i])*(1/2)*laplace_GTO(a_i[j]) * J(), -np.inf, np.inf)
-#     return T
+    for i in range(N):
+        for j in range(i+1, N):
+            r_i, r_j=np.array(molecule[i]["coord"]), np.array(molecule[j]["coord"])
+            Z_i, Z_j=symbol_to_Z[molecule[i]["symbol"]], symbol_to_Z[molecule[j]["symbol"]]
+            V+=Z_i*Z_j/(np.linalg.norm(r_i-r_j))
+    
+    return V
 
-# #核引力項の計算
-# def V_ne(N, a_i):
-#     def xi(a):
-#         return lambda x, y, z:(2*a/np.pi)**(3/4)*np.exp(-a*(x**2+y**2+z**2))
-#     T=np.zeros((N, N))
-#     for i in range(N):
-#         for j in range(i, N):
 
-# #コアハミルトニアン行列Hの計算
+K=len(molecule_basis) #基底関数の数
+
+def Nm_c(a): #GTOの規格化定数
+    return (2*a/np.pi)**(3/4)
+
+#==========step2:正準直交化の実行==========
+# 重なり行列Sの計算
+def S(molecule_basis):
+    S=np.zeros((K, K))
+
+    def S_prim(a:float, b:float, A:np.ndarray, B:np.ndarray): #原始GTO同士の重なり積分←多中心積分の公式とガウス積分から
+        return (np.pi/(a+b))**(3/2) * np.exp(-a*b/(a+b)*np.linalg.norm(A-B)**2)
+
+    for m in range(K):
+        for n in range(m, K):
+            alpha_m, alpha_n=molecule_basis[m]["exponents"], molecule_basis[n]["exponents"]
+            c_m, c_n=molecule_basis[m]["coefficients"], molecule_basis[n]["coefficients"]
+
+            A_m=molecule_basis[m]["center"]
+            A_n=molecule_basis[n]["center"]
+
+            s=0
+
+            for i in range(len(alpha_m)):
+                for j in range(len(alpha_n)):
+                    s+=c_m[i]*c_n[j]*Nm_c(alpha_m[i])*Nm_c(alpha_n[j])*S_prim(alpha_m[i], alpha_n[j], A_m, A_n)
+            
+            S[n][m]=s
+            S[m][n]=s
+    
+    return S
+
+
+
+#Sの対角化
+val_S, vec_S=np.linalg.eigh(S(molecule_basis))
+
+val_S_inv_sqrt = 1.0 / np.sqrt(val_S)
+s_inv_sqrt=np.diag(val_S_inv_sqrt, 0)
+U=vec_S
+
+
+#変換行列Xの計算(正準直交化)                 
+X=U@s_inv_sqrt
+
+
+
+
+
+#==========step3==========
+#運動エネルギー項Tの計算
